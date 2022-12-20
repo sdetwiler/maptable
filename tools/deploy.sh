b97575c9b552811a4243a9d6859cf097b9849c6a
Stephen Detwiler <steve@n3twork.com>
2022-12-20 09:24:31 -0800

    On deploy, only invalidate the homepage and map manifest to avoid Cloudfront charges by default.

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -7,4 +7,5 @@
 
 aws s3 sync ../site/ s3://www.piratestudios.com/maptable --exclude .DS_Store --profile piratestudios
 
-aws cloudfront create-invalidation --distribution-id E15MKJJIEOCOF6 --paths '/maptable/*' --profile piratestudios
+# By default only invalidate the homepage and the map manifest. Additional changes need manual invalidations.
+aws cloudfront create-invalidation --distribution-id E15MKJJIEOCOF6 --paths '/maptable/index.html' '/maptable/assets/css/style.css' '/maptable/assets/tiles/manifest.json' --profile piratestudios
